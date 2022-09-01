2af536555ed6f4ffd1bfef47b404d53609a7c62f
ZEUS_XD <100292162+KiaraxZeus@users.noreply.github.com>
2022-09-01 22:30:49 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -24,7 +24,7 @@ API_HASH = getenv("API_HASH", "f190067c436aa20ad21e99d554d508e9")
 BOT_TOKEN = getenv("BOT_TOKEN", "5511696527:AAHrwRwt1VMYa30-3HM6RDp9pw7o88x-OaI")
 
 # Database to save your chats and stats... Get MongoDB:-  https://telegra.ph/How-To-get-Mongodb-URI-04-06
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Zeusop:zeusop05@cluster0.pspgfqk.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://zeus:ZEUS@cluster0.bkwpngj.mongodb.net/?retryWrites=true&w=majority")
 
 if MONGO_DB_URI != None:
     MONGO_DB_URI = MONGO_DB_URI.strip()
