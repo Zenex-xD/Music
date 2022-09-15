5d6fcc7f20342bc3649e8483b3a5ef1adc1243e8
ZEUS_XD <100292162+KiaraxZeus@users.noreply.github.com>
2022-09-15 11:31:59 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -59,7 +59,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 # For customized or modified Repository
-UPSTREAM_REPO = "https://github.com/zeusop01/KiaraxMusic"
+UPSTREAM_REPO = "https://github.com/KiaraxZeus/KiaraxMusic"
 UPSTREAM_BRANCH = "main"
 
 # GIT TOKEN ( if your edited repo is private)
