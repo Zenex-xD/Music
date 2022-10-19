761b307950a638f0f9aecbdfd8446fe1a489b96c
mysterious-vishu <100484120+mysterious-vishu@users.noreply.github.com>
2022-10-19 17:02:10 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -159,7 +159,7 @@ autoclean = []
 
 
 # Images
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/e17f42bc195635b668d6d.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/a5b4b8ff1269794e9a1ef.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
